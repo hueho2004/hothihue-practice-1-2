f9e359501623fb77df3bacb6c7b9d8d73828f46d
hueho2004 <hue.ho25@student.passerellesnumeriques.org>
2024-04-07 21:42:58 +0700

    finish practice 3

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::put('/posts/{id}', [PostController::class, 'update']);
-Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/{id}',[PostController::class,'show']);
+Route::post('/posts',[PostController::class,'store']);
+Route::delete('/posts/{id}',[PostController::class,'update']);
+Route::put('/posts/{id}',[PostController::class,'destroy']);
 
 Route::get('/users',[UserController::class,'index']);
 Route::get('/users/{id}',[UserController::class,'show']);

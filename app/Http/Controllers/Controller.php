7d27258dca301dd0f273f138528a736bcbed4ec9
hueho2004 <hue.ho25@student.passerellesnumeriques.org>
2024-04-08 23:54:30 +0700

    fix swagger practice 2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
+
 abstract class Controller
 {
     //
+    /**
+ * @OA\Info(
+ *    title="Swagger with Laravel",
+ *    version="1.0.0",
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="bearerAuth",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+
+ */
 }
